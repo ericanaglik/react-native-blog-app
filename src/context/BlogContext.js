ef2ddc79da1986f2f603fcfb785478daaf9f1dde
erica2605@live.com <erica2605@live.com>
2020-06-02 15:30:25 -0400

    🌟 [updated] refetching to api after creating post

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,9 +1,10 @@
-import React, { useReducer } from 'react'
 import createDataContext from './createDataContext'
-
+import jsonServer from '../api/jsonServer'
 
 const blogReducer = (state, action) => {
   switch (action.type) {
+    case 'get_blogposts':
+      return action.payload
     case 'edit_blogpost':
       return state.map((blogPost) => {
         return blogPost.id === action.payload.id
@@ -19,9 +20,18 @@ const blogReducer = (state, action) => {
   }
 }
 
+const getBlogPosts = dispatch => {
+  return async () => {
+    const response = await jsonServer.get('/blogposts')
+
+    dispatch({ type: 'get_blogposts', payload: response.data })
+  }
+}
+
 const addBlogPost = dispatch => {
-  return (title, content, callback) => {
-    dispatch({ type: 'add_blogpost', payload: { title, content } })
+  return async (title, content, callback) => {
+    await jsonServer.post('/blogposts', { title, content })
+
     if (callback) {
       callback()
     }
@@ -43,4 +53,4 @@ const editBlogPost = dispatch => {
   }
 }
 
-export const { Context, Provider } = createDataContext(blogReducer, { addBlogPost, deleteBlogPost, editBlogPost }, [])
+export const { Context, Provider } = createDataContext(blogReducer, { addBlogPost, deleteBlogPost, editBlogPost, getBlogPosts }, [])
